c9a2db9a653d5064ea3891ddd649d17af0e52adf
jasiel <jasiel_serra@hotmail.com>
2021-09-07 15:00:28 -0300

    Criado e removendo atributo dinâmico (Em tempo de execução) em Pessoa.

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,5 +1,5 @@
 class Pessoa:
-    def __init__(self, *filhos,nome=None, idade = 35):
+    def __init__(self, *filhos, nome=None, idade = 35):
         self.nome = nome
         self.idade = idade
         self.filhos = list(filhos)
@@ -8,14 +8,17 @@ class Pessoa:
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    sofia = Pessoa(nome='Sofia', idade=0.8)
-    lis = Pessoa(nome='Liz', idade=0.8)
-    sara = Pessoa(sofia,lis,nome='Sara')
+    sofia = Pessoa(nome='Sofia', idade='8 meses')
+    lis = Pessoa(nome='Liz', idade='8 meses')
+    sara = Pessoa(sofia, lis, nome='Sara')
     print(Pessoa.cumprimentar(sara))
     print(id(sara))
     print(sara.cumprimentar())
     print(sara.idade)
+    print(sara.nome)
     for filhos in sara.filhos:
         print(filhos.nome, filhos.idade)
-    print(sara.nome)
-    print(sara.idade)
+    sara.sobrenome = 'Serra'
+    print(sara.__dict__)
+    del sara.filhos
+    print(sara.__dict__)
